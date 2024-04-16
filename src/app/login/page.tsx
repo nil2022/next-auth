89df8fd1fb41db33d9ad5a63cbd348b7502e40f3
Nilanjan Haldar <nil.haldar@gmail.com>
2024-04-17 01:02:28 +0530

    middlewares, bugs fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React, {useEffect} from "react";
 import {useRouter} from "next/navigation";
 import axios from "axios";
-import { toast } from "react-hot-toast";
+import { toast, Toaster } from "react-hot-toast";
 
 export default function LoginPage() {
 
@@ -28,7 +28,7 @@ export default function LoginPage() {
             toast.success("Login success");
             router.push("/profile");
         } catch (error:any) {
-            console.log("Login failed", error.message);
+            console.log("Login failed", error);
             toast.error(error.message);
         } finally{
         setLoading(false);
@@ -45,6 +45,7 @@ export default function LoginPage() {
 
     return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Toaster />
         <h1>{loading ? "Processing" : "Login"}</h1>
         <hr />
         
